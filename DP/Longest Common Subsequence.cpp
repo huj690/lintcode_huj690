700b1d64fb4640ab0bb9feab3a1f3c272a51cb7e
Jin Hu <jin.hu2013@gmail.com>
2015-01-31 15:52:59 +0800

    Update Longest Common Subsequence.cpp

--- DP/Longest Common Subsequence.cpp
+++ DP/Longest Common Subsequence.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:    
-    int longestCommonSubstring(string &A, string &B) {
+    int longestCommonSubsequence(string A, string B) {
         if (A.empty() || B.empty()) {
             return 0;
         }
         
-		int i_index, j_index;
+        int i_index, j_index;
         vector<vector<int> > dp(A.length() + 1, vector<int> (B.length() + 1, 0));
         for (int i = 1; i <= A.length(); i++) {
             for (int j = 1; j <= B.length(); j++) {
@@ -25,32 +25,27 @@ public:
 //优化为只有2 * n行而不是m * n
 class Solution {
 public:    
-    /**
-     * @param A, B: Two string.
-     * @return: the length of the longest common substring.
-     */
-    int longestCommonSubstring(string &A, string &B) {
-        // write your code here
+    int longestCommonSubsequence(string A, string B) {
         if (A.empty() || B.empty()) {
             return 0;
         }
         
-		int i_index, j_index, maxCnt = 0;
+	int i_index, j_index;
         vector<int> dp_prev(B.length() + 1, 0);
         vector<int> dp(B.length() + 1, 0);
         
-        for (int i = 1; i <= A.length(); i++) { 
+        for (int i = 1; i <= A.length(); i++) {
             for (int j = 1; j <= B.length(); j++) {
-                i_index = i - 1;
-                j_index = j - 1;
-                if (A[i_index] == B[j_index]) {
-                    dp[j] = dp_prev[j - 1] + 1;
-                    maxCnt = max(dp[j], maxCnt);
-                }
+				i_index = i - 1;
+				j_index = j - 1;
+                dp[j] = max(dp_prev[j], 
+                           max (dp[j - 1], 
+                           A[i_index] == B[j_index] ? dp_prev[j - 1] + 1 : dp_prev[j - 1]));
             }
             dp_prev = dp;
             dp.assign(B.length() + 1, 0);
         }
-        return maxCnt;
+        
+        return dp_prev[B.length()];
     }
 };

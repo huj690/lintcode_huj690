a2fb56a49d9fd37bbcdef41547eb376d8155a948
Jin Hu <jin.hu2013@gmail.com>
2015-02-03 11:05:05 +0800

    Update QuickSort.cpp

--- Sort/QuickSort.cpp
+++ Sort/QuickSort.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
-  void qsort(vector<int> &num, int begin, int end) {
-  	if (begin < end) {
-  		int index = partition(num, begin, end);
-  		qsort(num, begin, index - 1);
-  		qsort(num, index + 1, end);
-	  }
-  }
-  
+	void qsort(vector<int> &num, int begin, int end) {
+		if (begin < end) {
+			int index = partition(num, begin, end);
+			qsort(num, begin, index - 1);
+			qsort(num, index + 1, end);
+		}
+	}
+
 private:
-  int partition(vector<int> &num, int begin, int end) {
-    int key = num[begin];
-    int i = begin, j = end;
-    while (i < j) {
-        while (i < j && num[j] > key) {
-            j--;
-        }
-        if (i < j) {
-            num[i] = num[j];
-            i++;
-        }
-        while (i < j && num[i] <= key) {
-            i++;
-        }
-        if (i < j) {
-            num[j] = num[i];
-            j--;
-        }
-    }
-    num[i] = key;
-    return i;
-    }
+	int partition(vector<int> &num, int begin, int end) {
+		int key = num[begin];
+		int i = begin, j = end;
+		while (i < j) {
+			while (i < j && num[j] > key) {
+				j--;
+			}
+			if (i < j) {
+				num[i] = num[j];
+				i++;
+			}
+			while (i < j && num[i] <= key) {
+				i++;
+			}
+			if (i < j) {
+				num[j] = num[i];
+				j--;
+			}
+		}
+		num[i] = key;
+		return i;
+	}
 
 };
